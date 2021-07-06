ce2f0ba074354416bdc11cc558aa07b357d40866
LiuChang <liuchangweb@163.com>
2021-07-06 16:56:54 +0800

    jsx basic study

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const text = 'Hello,React';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // 虚拟 DOM 只能有一个根标签并且不需要引号
+    <div className="App"> {/* 样式类名指定不要用 class，要使用 className。 */}
+      <span style={{ fontSize: '24px' }}>{text}</span> {/* 内联样式要用对象形式写。 */}
+      <br /> {/* 标签必须闭合 */}
+      <p>这是一个小写字母</p>
+      {/* <P>这里会报错，因为找不到对应的组件。</P> */}
     </div>
   );
 }
