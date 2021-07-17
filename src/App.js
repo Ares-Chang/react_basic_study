f29af3d47f580c173132f1231187d58779819ed8
LiuChang <liuchangweb@163.com>
2021-07-17 15:21:48 +0800

    add: 回调型 Refs Demo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types'
 
-// String 型 Refs Demo
+// 回调型 Refs Demo
 class App extends React.Component {
   showText = () => {
-    // 通过 this.refs 调用
-    const { input } = this.refs
-    alert(input.value)
+    alert(this.input.value)
+  }
+  setDome = (e) => {
+    this.input = e
   }
   render() {
     return (
       <div>
-        {/* 直接 ref='name' */}
-        <input ref="input" onBlur={this.showText} type="text" placeholder="失去焦点弹窗输入内容！" />
+        {/* class 绑定式 */}
+        <input ref={this.setDome} onBlur={this.showText} type="text" placeholder="失去焦点弹窗输入内容！" />
       </div>
     )
   }
