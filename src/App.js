f1b28f4d58dbe6f56ca1c9611152dadd2fa01417
LiuChang <liuchangweb@163.com>
2021-07-20 15:18:01 +0800

    add: 非受控组件

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import PropTypes from 'prop-types'
 
-// createRef Demo
+// 非受控组件
 class App extends React.Component {
-  myRef = React.createRef()
-  showText = () => {
-    alert(this.myRef.currenst.value)
+  handleButton = (e) => {
+    e.preventDefault()  // 阻止 form 跳转
+    const { userName, passWord } = this
+    console.log(`你输入的用户名为：${userName.value}，输入的密码为：${passWord.value}`)
   }
+
   render() {
     return (
-      <div>
-        {/* createRef 绑定式 */}
-        <input ref={this.myRef} onBlur={this.showText} type="text" placeholder="失去焦点弹窗输入内容！" />
-      </div>
+      <form onSubmit={this.handleButton}>
+        用户名：<input ref={e => this.userName = e} type="text" />
+        密码：<input ref={e => this.passWord = e} type="password" />
+        <button>提交</button>
+      </form>
     )
   }
 }
