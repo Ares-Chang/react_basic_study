154349151c5c3956dc16df0296a7bbb2b71505ab
LiuChang <liuchangweb@163.com>
2021-07-20 15:33:19 +0800

    add: 受控组件

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React from 'react';
 
-// 非受控组件
+// 受控组件
 class App extends React.Component {
+  state = {
+    userName: '',
+    passWord: ''
+  }
+
   handleButton = (e) => {
     e.preventDefault()  // 阻止 form 跳转
-    const { userName, passWord } = this
-    console.log(`你输入的用户名为：${userName.value}，输入的密码为：${passWord.value}`)
+    const { userName, passWord } = this.state
+    console.log(`你输入的用户名为：${userName}，输入的密码为：${passWord}`)
   }
 
   render() {
     return (
       <form onSubmit={this.handleButton}>
-        用户名：<input ref={e => this.userName = e} type="text" />
-        密码：<input ref={e => this.passWord = e} type="password" />
+        {/* 动态更改数据状态，只要用户触发，就会更改 */}
+        用户名：<input onChange={(e) => this.setState({ userName: e.target.value })} type="text" />
+        密码：<input onChange={(e) => this.setState({ passWord: e.target.value })} type="password" />
         <button>提交</button>
       </form>
     )
